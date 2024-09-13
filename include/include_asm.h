ce1ead7d34906c9d977ae050ad6a267e8000756e
Ziemas <ziemas@ziemas.se>
2024-09-13 03:36:58 +0200

    fix settings

--- include/include_asm.h
+++ include/include_asm.h
@@ -3,28 +3,28 @@
 
 #if !defined(SPLAT) && !defined(M2CTX) && !defined(PERMUTER)
 #ifndef INCLUDE_ASM
-#define INCLUDE_ASM(FOLDER, NAME) \
-    __asm__( \
-        ".section .text\n" \
-        "    .set noat\n" \
-        "    .set noreorder\n" \
-        "    .globl " #NAME ".NON_MATCHING\n" \
-        "    .type " #NAME ".NON_MATCHING, @object\n" \
-        "    " #NAME ".NON_MATCHING:\n" \
-        "    .include \"" FOLDER "/" #NAME ".s\"\n" \
-        "    .set reorder\n" \
-        "    .set at\n" \
-    )
+#define INCLUDE_ASM(FOLDER, NAME)                           \
+    __asm__(                                                \
+        ".section .text\n"                                  \
+        "   .set noat\n"                                    \
+        "   .set noreorder\n"                               \
+        "   .globl\t" #NAME ".NON_MATCHING\n"               \
+        "   .ent\t" #NAME "\n" #NAME ".NON_MATCHING:\n"     \
+        "   .include \"" FOLDER "/" #NAME ".s\"\n"          \
+        "   .set reorder\n"                                 \
+        "   .set at\n"                                      \
+        "   .end\t" #NAME                                   \
+    );
 #endif
 #ifndef INCLUDE_RODATA
-#define INCLUDE_RODATA(FOLDER, NAME) \
-    __asm__( \
-        ".section .rodata\n" \
-        "    .include \"" FOLDER "/" #NAME ".s\"\n" \
-        ".section .text" \
+#define INCLUDE_RODATA(FOLDER, NAME)                        \
+    __asm__(                                                \
+        ".section .rodata\n"                                \
+        "    .include \"" FOLDER "/" #NAME ".s\"\n"         \
+        ".section .text"                                    \
     )
 #endif
-__asm__(".include \"include/labels.inc\"\n");
+__asm__(".include \"include/macro.inc\"\n");
 #else
 #ifndef INCLUDE_ASM
 #define INCLUDE_ASM(FOLDER, NAME)
@@ -34,4 +34,4 @@ __asm__(".include \"include/labels.inc\"\n");
 #endif
 #endif
 
-#endif
+#endif /* INCLUDE_ASM_H */

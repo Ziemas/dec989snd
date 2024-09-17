e1f841f3471f09d1dc59e12dc6dbcb31641f879d
Ziemas <ziemas@ziemas.se>
2024-09-17 05:38:09 +0200

    start working on ame.c

---
+++ src/ame.c
@@ -0,0 +1,113 @@
+#include "common.h"
+#include "functions.h"
+#include "globals.h"
+
+INCLUDE_ASM("asm/nonmatchings/ame", snd_PlayAMESound);
+//UInt32 snd_PlayAMESound(MIDISoundPtr sound, SInt32 vol, SInt32 pan, SInt16 pitch_mod, int bend) {
+//	SInt16 bend;
+//	AMEHandlerPtr stream_handler;
+//	MultiMIDIBlockHeaderPtr ame_master;
+//}
+
+INCLUDE_ASM("asm/nonmatchings/ame", snd_StartAMESegment);
+//SInt32 snd_StartAMESegment(AMEHandlerPtr parent, SInt32 segment) {
+//	GSoundHandlerPtr walk;
+//	AMEStreamHeaderPtr stream_ptr;
+//	MultiMIDIBlockHeaderPtr ame_master;
+//	MIDISoundPtr m_sound;
+//}
+
+INCLUDE_ASM("asm/nonmatchings/ame", snd_StopAMESegment);
+//void snd_StopAMESegment(MIDIHandlerPtr handler) {}
+
+INCLUDE_ASM("asm/nonmatchings/ame", snd_FindAMEStream);
+//AMEStreamHeaderPtr snd_FindAMEStream(MIDIHandlerPtr stream1, SInt32 index) {
+//	MultiMIDIBlockHeaderPtr ame_master;
+//}
+
+INCLUDE_ASM("asm/nonmatchings/ame", snd_SetAMESoundVolumePan);
+//void snd_SetAMESoundVolumePan(UInt32 handle, SInt32 vol, SInt32 pan) {
+//	AMEHandlerPtr stream;
+//	MIDIHandlerPtr walk;
+//}
+
+INCLUDE_ASM("asm/nonmatchings/ame", snd_SetAMESoundPitchModifier);
+//void snd_SetAMESoundPitchModifier(UInt32 handle, SInt16 mod) {
+//	AMEHandlerPtr stream;
+//	MIDIHandlerPtr walk;
+//}
+
+INCLUDE_ASM("asm/nonmatchings/ame", snd_AMEFunction);
+//SInt8* snd_AMEFunction(MIDIHandlerPtr stream, UInt8 *ame_header) {
+//	SInt32 compare_value;
+//	UInt8 *command_ptr;
+//	SInt16 next_command;
+//	SInt32 ignore_next_command;
+//	AMEStreamHeaderPtr stream_to_stop;
+//	SInt32 y;
+//	SInt32 group;
+//	SInt32 counter;
+//	SInt32 done;
+//	SInt32 stop;
+//	SInt8 *stream_end;
+//	AMEHandlerPtr ame_handler;
+//}
+
+void snd_SetMIDIRegister(UInt32 handle, SInt32 reg, SInt16 value) {
+	AMEHandlerPtr ame;
+    if (reg < 0 || reg >= 16) {
+        snd_ShowError(92, reg, value, 0, 0);
+        return;
+    }
+
+    snd_LockMasterTick(10);
+
+    if (((handle >> 24) & 0x1f) != 2 || !(ame = snd_CheckHandlerStillActive(handle))) {
+        snd_UnlockMasterTick();
+        return;
+    }
+
+    if (value >= 128) {
+        value = 127;
+    }
+
+    if (value < 0) {
+        value = 0;
+    }
+
+    ame->MIDIRegister[reg] = value;
+
+    snd_UnlockMasterTick();
+}
+
+void snd_SetAllMIDIRegister(UInt32 handle, SInt8 *vals) {
+	AMEHandlerPtr ame;
+	SInt32 i;
+
+    snd_LockMasterTick(11);
+
+    if (((handle >> 24) & 0x1f) != 2 || !(ame = snd_CheckHandlerStillActive(handle))) {
+        snd_UnlockMasterTick();
+        return;
+    }
+
+    for (i = 0; i < 16; i++) {
+        ame->MIDIRegister[i] = vals[i]; 
+    }
+    
+    snd_UnlockMasterTick();
+}
+
+SInt32 snd_GetMIDIRegister(UInt32 handle, SInt32 reg) {
+	AMEHandlerPtr ame;
+
+    if (((handle >> 24) & 0x1f) != 2) {
+        return 0;
+    }
+
+    if (!(ame = snd_CheckHandlerStillActive(handle))) {
+        return 0;
+    }
+
+    return ame->MIDIRegister[reg];
+}
